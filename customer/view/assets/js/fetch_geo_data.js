793323fe551b9d7bd66bb3acd051745d44c75dc3
dpnam2112 <dpnam2112@gmail.com>
2023-12-01 23:38:16 +0700

    Commit missing file: fetch_geo_data.js

--- customer/view/assets/js/fetch_geo_data.js
+++ customer/view/assets/js/fetch_geo_data.js
@@ -42,3 +42,46 @@ document.getElementById("user-info__select-province").addEventListener("change",
             selectDistrict.value = "";
         })
 })
+
+document.getElementById("checkout-complete").addEventListener("click", () => {
+    // Assuming you have a <select> element with the id "mySelect"
+    let provinceNode = document.getElementById("user-info__select-province");
+    let districtNode = document.getElementById("user-info__select-district");
+
+    // Get the selected option
+    let prov = provinceNode.options[provinceNode.selectedIndex].innerText;
+    let distr = districtNode.options[provinceNode.selectedIndex].innerText;
+    let fullName = document.getElementById('full-name').value;
+    let phoneNumber = document.getElementById('phone-number').value;
+    let addr = document.getElementById('address').value;
+    let paymentMethod = document.querySelector('input[name="payment-method"]:checked').value;
+
+    payload = {
+        province: prov,
+        district: distr,
+        full_name: fullName,
+        phone_number: phoneNumber,
+        address: addr,
+        payment: paymentMethod
+    }
+
+    console.log(payload);
+
+    fetch('checkout.php', {
+        method: 'POST',
+        cors: true,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    }).then((res) => {
+        if (res.status == 200) {
+            var successModal = document.getElementById("checkout-success");
+            successModal.style.display = "block";
+        } else 
+            var failModal = document.getElementById("checkout-failed");
+            failModel.style.display = "block";
+        }
+    )
+})
+
