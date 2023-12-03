1ce0cb764d50479782729899bfefe2cdaf1a9a86
Nguyễn Đại Tiến <110327876+tien2114988@users.noreply.github.com>
2023-12-03 16:29:18 +0700

    Update header.php

--- customer/view/partials/header.php
+++ customer/view/partials/header.php
@@ -64,7 +64,7 @@ else {
                             <a class="p-0 me-5 nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> NAM </a>
                             <ul class="dropdown-menu border-0 pb-0">
                                 <?php
-                                $categories = $category_model->get_category_by_object("1");
+                                $categories = $category_model->get_category_by_object("Nam");
                                 foreach ($categories as $category) { ?>
                                     <li class="border"><a class="dropdown-item" href="../app/index.php?item_list&&category_id=<?php echo $category['id'] ?>"><?php echo $category['category_name']; ?></a></li>
                                 <?php } ?>
@@ -74,7 +74,7 @@ else {
                             <a class="p-0 me-5 nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> NỮ </a>
                             <ul class="dropdown-menu border-0 pb-0">
                                 <?php
-                                $categories = $category_model->get_category_by_object("2");
+                                $categories = $category_model->get_category_by_object("Nữ");
                                 foreach ($categories as $category) { ?>
                                     <li class="border"><a class="dropdown-item" href="../app/index.php?item_list&&category_id=<?php echo $category['id']; ?>"><?php echo $category['category_name']; ?></a></li>
                                 <?php } ?>
@@ -84,7 +84,7 @@ else {
                             <a class="p-0 me-5 nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> BÉ TRAI </a>
                             <ul class="dropdown-menu border-0 pb-0">
                                 <?php
-                                $categories = $category_model->get_category_by_object("3");
+                                $categories = $category_model->get_category_by_object("Bé Trai");
                                 foreach ($categories as $category) { ?>
                                     <li class="border"><a class="dropdown-item" href="../app/index.php?item_list&&category_id=<?php echo $category['id']; ?>"><?php echo $category['category_name']; ?></a></li>
                                 <?php } ?>
@@ -94,7 +94,7 @@ else {
                             <a class="p-0 me-5 nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> BÉ GÁI </a>
                             <ul class="dropdown-menu border-0 pb-0">
                                 <?php
-                                $categories = $category_model->get_category_by_object("4");
+                                $categories = $category_model->get_category_by_object("Bé Gái");
                                 foreach ($categories as $category) { ?>
                                     <li class="border"><a class="dropdown-item" href="../app/index.php?item_list&&category_id=<?php echo $category['id']; ?>"><?php echo $category['category_name']; ?></a></li>
                                 <?php } ?>
